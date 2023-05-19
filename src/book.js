b3a8fb07d11edbe3dacd3954b43facf63810f33d
Gauri Joshi <gauri.joshi@planetargon.com>
2023-05-19 15:08:09 -0700

    Test: book.js can create a title, build character object, and save unique reviews

--- src/book.js
+++ src/book.js
@@ -1,11 +1,29 @@
-function createTitle() {
-  
+function createTitle(title) { // createTitle is the function name. It's taking in a title.
+  var modifiedTitle = "The " + title; // variable modifiedTitle does the following: it prepends the word The and concatenates the title it's taking in.
+
+  return modifiedTitle; // return the above variable.
 }
 
+function buildMainCharacter(name, age, pronouns) { // similar to ruby class this is initializing a character object; the attributes here are name, age, character.
+  var mainCharacter = {
+      name: name,
+      age: age,
+      pronouns: pronouns
+    };
+
+return mainCharacter; // return the character object
+}
+
+function saveReview(review, reviews) {
+
+  reviews.push(review); // i don't have to set reviews equal to an empty array like in ruby?
+
+
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
--- src/warm-up.js
+++ src/warm-up.js
@@ -10,8 +10,8 @@
 // Initialize two new variables, "title", and "pages", and assign values to them with
 // a book's title and page count.
 
-// var title = "Harry Potter";
-// var ...
+var title = "Harry Potter";
+var pages = 450;
 
 
 
@@ -19,21 +19,23 @@
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is 
 
-
+pages *= .25;
 
 // Initialize a new variable "movie", but leave it undefined
 
+var movie;
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
+var sequel = `${title} Part Two`
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
-
+var isFavorite = true;
 
 
 // Create a new variable named "isShortStory", and use a comparison operator to
@@ -41,6 +43,7 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+var isShortStory = (pages < 25) ? "True":"False";
 
 
 //------------------------- Conditionals -------------------------//
